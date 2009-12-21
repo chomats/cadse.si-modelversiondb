1e53e397144ca2ac19953db27b5a8fc6b1ea7f2e
Stephane Chomat <Stephane.Chomat@imag.fr>
2009-12-21 17:30:52 +0100

    change use UUID

---
+++ src/fr/imag/adele/teamwork/db/impl/SQLTypes.java
@@ -0,0 +1,46 @@
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package fr.imag.adele.teamwork.db.impl;
+
+public class SQLTypes {
+	
+	public static final String BOOLEAN_SQL_TYPE_DEF = "BOOLEAN";
+
+	public static final String INT_SQL_TYPE_DEF = "INT";
+	
+	public static final String BIGINT_SQL_TYPE_DEF = "BIGINT";
+	
+	public static final String VARCHAR2_SQL_TYPE_DEF = "VARCHAR2";
+
+	public static final String BLOB_SQL_TYPE_DEF = "BLOB";
+	
+	public static final String DATE_SQL_TYPE_DEF = "DATE";
+	
+	public static final String LONG_SQL_TYPE_DEF = "LONG";
+
+	public static final String TIME_SQL_TYPE_DEF = "TIME";
+	
+	public static final String CLOB_SQL_TYPE_DEF = "CLOB";
+	
+	public static final String OBJ_SQL_TYPE_DEF = "OBJECT";
+
+	public static final String OTHER_SQL_TYPE_DEF = "OTHER";
+
+	public static final String LONGVARBIN_SQL_TYPE_DEF = "LONGVARBINARY";
+}
